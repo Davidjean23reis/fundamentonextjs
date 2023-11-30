e98ac0743e2c024bc18c29600f30b57871206e37
David <david.jean.reis23@gmail.com>
2023-11-30 17:55:01 -0300

    Criei Rotas

---
+++ src/pages/OutrasRotas.jsx
@@ -0,0 +1,7 @@
+export default function OutrasRotas(){
+    return (
+       <div>
+        <h1>Outras Rotas</h1>
+       </div>
+    )
+}
---
+++ src/pages/rotas.tsx
@@ -0,0 +1,7 @@
+export default function rotas(){
+    return (
+       <div>
+        <h1>Olá mundo</h1>
+       </div>
+    )
+}
