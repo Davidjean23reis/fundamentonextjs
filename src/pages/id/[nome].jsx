130db14f8cc73d182cb256c705135862b1e0d558
David <david.jean.reis23@gmail.com>
2023-11-30 19:12:12 -0300

    Rotas 01

---
+++ src/pages/id/[nome].jsx
@@ -0,0 +1,8 @@
+import {userRouter} from'next/router'
+export default function codigoEnome(){
+    return (
+       <div>
+        <h1> Rotas</h1>
+       </div>
+    )
+}
---
+++ src/pages/id/buscar.tsx
@@ -0,0 +1,7 @@
+export default function buscar(){
+    return (
+       <div>
+        <h1> Rotas/ buscar</h1>
+       </div>
+    )
+}
