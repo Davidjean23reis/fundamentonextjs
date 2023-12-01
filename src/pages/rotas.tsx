d166e50adac2a54e54f83aecd395f71310f6e968
David <david.jean.reis23@gmail.com>
2023-11-30 22:37:00 -0300

    Navegando entre Páginas 02

--- src/pages/rotas.tsx
+++ src/pages/rotas.tsx
@@ -1,6 +1,21 @@
 import Link from 'next/link'
+import Router, { useRouter } from 'next/router'
 
 export default function rotas() {
+
+    function navegacaoSimples(url) {
+        Router.push(url)
+    }
+
+    function navegacaoComParams() {
+        Router.push({
+            pathname: "/codigo/rotasparams?id=12&nome=Ana",
+            query: {
+                id: 123,
+                nome: 'Ana'
+            }
+        })
+    }
     return (
         <div>
             <h1>Rota index</h1>
@@ -12,6 +27,11 @@ export default function rotas() {
                 <Link href="/codigo/nome">
                     <li>David</li> </Link>
             </ul>
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
+                <button onClick={navegacaoComParams}>Param</button>
+                <button onClick={() => Router.push("/codigo/nome")}>Buscar</button>
+                <button onClick={() => navegacaoSimples("/codigo/buscar")}>David</button>
+            </div>
         </div>
     )
 }
